69d957f154160ab6a6e6c77f56b70d69f151ea2b
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-01-25 11:02:48 -0500

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Container from "../components/container";
+
+const About = () => {
+  return (
+    <Container>
+      <h1>About</h1>
+      <p>lorem ipsum</p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ pages/services.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Container from "../components/container";
+
+const Services = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Next.js Project - Services</title>
+      </Head>
+      <h1>Services</h1>
+      <p>lorem ipsum </p>
+    </Container>
+  );
+};
+
+export default Services;
